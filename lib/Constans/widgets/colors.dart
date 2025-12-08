632c3c4934d1175dc0f152bfee24709ab0b43507
Youssef <Youssedhamed7@gmail.com>
2025-12-08 14:10:04 +0200

    colors code

--- lib/Constans/widgets/colors.dart
+++ lib/Constans/widgets/colors.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
-// ignore: camel_case_types
-class mainColors{
-  final Color primary = Color(0xff085651);
-  final Color titleColor = Color(0xff2E3A38);
-  final Color backgroundColor = Color(0xffEEEEEE);
-  final Color buttonColor = const Color(0xFF00695C);
+
+class AppColorScheme {
+  final Color primary;
+
+  final Color backgroundColor;
+  final Color buttonColor;
+
+  const AppColorScheme({
+    required this.primary,
+
+    required this.backgroundColor,
+    required this.buttonColor,
+  });
 }
 
+class AppColors {
+  // -------------------------
+  // LIGHT THEME COLORS
+  // -------------------------
+  static final AppColorScheme light = AppColorScheme(
+    primary: const Color(0xff085651),
+    backgroundColor: const Color(0xffB4B4B4).withOpacity(0.3),
+    buttonColor: const Color(0xFF00695C),
+  );
 
+  // -------------------------
+  // DARK THEME COLORS
+  // -------------------------
+  static final AppColorScheme dark = AppColorScheme(
+    primary: const Color(0xFF1F3A35),
+    backgroundColor: const Color(0xFF061514),
+    buttonColor: const Color(0xFF1F3A35),
+  );
+}
