5e596999a83fb689bbe884c03e097fa7fc1b7f11
Youssef <Youssedhamed7@gmail.com>
2025-12-09 13:43:50 +0200

    Add dark theme support for login screens

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:intershipflutter/businessLogic/home%20provideres/offer_provider.
 import 'package:intershipflutter/businessLogic/restaurant%20provider/restaurant_provider.dart';
 import 'package:intershipflutter/businessLogic/user%20provider/user_provider.dart';
 import 'package:provider/provider.dart';
-
+// 
 void main() {
   runApp(
     MultiProvider(
